cdd275548472ee86fd6f07950f58be9cbdd3a364
Cpt_Crunch <jasong84@gmail.com>
2019-12-02 19:24:43 -0600

    Relocate solutions into typescript directory

--- typescript/day2/part_one.ts
+++ typescript/day2/part_one.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { parse } from './parse';
 import { compute } from './compute';
 
-const file = fs.readFileSync('./day2/puzzle.txt','utf8');
+const file = fs.readFileSync('./typescript/day2/puzzle.txt','utf8');
 const data = parse(file)//?
 const reader = compute(data, 12, 2, false);
 console.log(reader)//?
