e6f31d86e2f04b44f0f897443219f74cda49f9f5
Cpt_Crunch <jasong84@gmail.com>
2019-12-06 22:24:28 -0600

    Add solution to Day 6 part 2

--- typescript/day6/solution.ts
+++ typescript/day6/solution.ts
@@ -11,6 +11,12 @@ const countOrbits = (node: Graph): number => {
     )
 }
 
-const solution1 = countOrbits(graph)
+const countOrbitTransfer = (node: Graph): number => {
+    return alg.dijkstra(node, "YOU", null, node.nodeEdges.bind(node)).SAN.distance
+}
 
+const solution1 = countOrbits(graph)
 solution1//?
+const YOUSAN = 2// This is the sum of edges between YOU and the object YOU orbit and the object  between SANTA and the object SANTA orbits
+const solution2 = countOrbitTransfer(graph) - YOUSAN
+solution2//?
