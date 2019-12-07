59436d513293a002946af619f4eb3e382263ada8
Cpt_Crunch <jasong84@gmail.com>
2019-12-06 21:38:12 -0600

    Add solution to Day 5 part 2

--- typescript/day5/part_one.ts
+++ typescript/day5/part_one.ts
@@ -7,14 +7,22 @@ enum OPCODES {
 	ADD = 1,
 	MULTIPLY,
 	INPUT,
-	OUTPUT,
+    OUTPUT,
+    JUMP_IF_TRUE,
+    JUMP_IF_FALSE,
+    LESS_THAN,
+    EQUALS,
 	HALT = 99,
 }
 enum Instructions {
 	ADD = 4,
 	MULTIPLY = 4,
 	INPUT = 2,
-	OUTPUT = 2,
+    OUTPUT = 2,
+    JUMP_IF_TRUE = 3,
+    JUMP_IF_FALSE = 3,
+    LESS_THAN = 4,
+    EQUALS = 4,
 }
 
 const file = fs.readFileSync('./data/dayFivePuzzle.txt','utf8');
@@ -31,12 +39,13 @@ const OBD = (source: string, inputs: number[]): number[] => {
 
         const init = String(program[pointer]).padStart(5, "0")
         const opcode = +init.substr(3)
-        if (opcode === OPCODES.HALT) break
+        if (opcode === OPCODES.HALT) { break }
         
 
         const params = init.substr(0, 3).split("").reverse().map(Number)
         const dx1 = getValue(program, params, pointer, 0)
         const dx2 = getValue(program, params, pointer, 1)
+        let shouldJump = true;
     
         switch (opcode) {
           case OPCODES.ADD: {
@@ -55,12 +64,35 @@ const OBD = (source: string, inputs: number[]): number[] => {
             outputs.push(dx1)
             break
           }
+          case OPCODES.JUMP_IF_TRUE: {
+              pointer = dx1 !== 0 ? dx2 : pointer
+              shouldJump = dx1 === 0
+              break
+          }
+          case OPCODES.JUMP_IF_FALSE: {
+              pointer = dx1 === 0 ? dx2: pointer
+              shouldJump = dx1 !== 0
+              break
+          }
+          case OPCODES.LESS_THAN: {
+              program[program[pointer + 3]] = dx1 < dx2 ? 1 : 0
+              break
+          }
+          case OPCODES.EQUALS: {
+              program[program[pointer + 3]] = dx1 === dx2 ? 1 : 0
+              break
+          }
         }
-          pointer += Instructions[OPCODES[opcode]]
-      }
 
-        return outputs
+        if(shouldJump){
+            pointer += Instructions[OPCODES[opcode]]
+        }
     }
 
-const resultA = last_(OBD(file, [1]))
-resultA//?
+    return outputs
+}
+
+const partOne = last_(OBD(file, [1]))
+partOne//?
+const partTwo = last_(OBD(file, [5]))
+partTwo//?
