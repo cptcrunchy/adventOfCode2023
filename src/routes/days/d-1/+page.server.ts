7374e4ac1136941e9484b8f212125f6531317d97
Cpt_Crunch <jasong84@gmail.com>
2023-12-01 16:12:44 -0600

    Day 1 Part 2

--- src/routes/days/d-1/+page.server.ts
+++ src/routes/days/d-1/+page.server.ts
@@ -1,4 +1,4 @@
-import {sampleOne } from "./sample";
+import {sampleOne, sampleTwo } from "./sample";
 import puzzleInput from "./puzzle";
 
 
@@ -9,11 +9,28 @@ function partOneCalibrationValue(data: string[]) {
     .reduce((prev, curr) => prev + curr)
 }
 
+function partTwoCalibrationValue(data: string[]) {
+	const numbersChart = /(\d|twone|sevenine|oneight|threeight|nineight|fiveight|eighthree|eightwo|one|two|three|four|five|six|seven|eight|nine)/gms
+	return data.map(row => row.match(numbersChart))  
+	.map(row => row.flatMap(v => (
+		{ 'twone': [2, 1], 'sevenine': [7, 9], 'oneight': [1, 8],
+			'threeight': [3, 8], 'nineight': [9, 8], 'fiveight': [5, 8],
+			'eighthree': [8, 3], 'eightwo': [8, 2],
+			one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9,
+			'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9
+		}[v])))  
+		.map(nums => +[nums[0], nums[nums.length - 1]].join(''))  
+		.reduce((a, b) => a + b, 0)
+}
+
+
 export const load = () => {
 	const rows = puzzleInput.split('\n');
 	const result = partOneCalibrationValue(rows);
+	const result2 = partTwoCalibrationValue(rows);
 	console.log(result);
+	console.log(result2);
 
-	return { rows };
+	return { result, result2 };
 }
 
